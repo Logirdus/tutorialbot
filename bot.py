43bdcba399064f281d8e8c539ad602c4f5c24039
Logird <regdust@gmail.com>
2020-09-08 22:22:01 +0300

    В хендлер help добавлено 3 кнопки

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import telebot, logging
 from variables import *
+from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
@@ -10,6 +11,12 @@ def start_handler(message):
 @bot.message_handler(commands=['help'])
 def help_handles(message):
     bot.send_message(message.from_user.id, help_mess)
+    key = types.InlineKeyboardMarkup()
+    but_1 = types.InlineKeyboardButton(text="Number one", callback_data="NumberOne")
+    but_2 = types.InlineKeyboardButton(text="Number Two", callback_data="NumberTwo")
+    but_3 = types.InlineKeyboardButton(text="Number Tree", callback_data="NumberTree")
+    key.add(but_1, but_2, but_3)
+    bot.send_message(message.chat.id, "Выберите кнопку", reply_markup=key)
 
 @bot.message_handler(func=lambda message:True)
 def forward_handles(message):
