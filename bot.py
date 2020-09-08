e09908f08260d9330009a4ae530711b95d6b39fd
Logird <regdust@gmail.com>
2020-09-08 22:24:10 +0300

    Добавлены ответы на нажатия кнопок в /help и структура меню при нажатии NumberTree

--- bot.py
+++ bot.py
@@ -4,9 +4,11 @@ from telebot import types
 
 bot = telebot.TeleBot(TOKEN)
 
+
 @bot.message_handler(commands=['start'])
 def start_handler(message):
     bot.send_message(message.from_user.id, start_mess)
+
 
 @bot.message_handler(commands=['help'])
 def help_handles(message):
@@ -17,6 +19,22 @@ def help_handles(message):
     but_3 = types.InlineKeyboardButton(text="Number Tree", callback_data="NumberTree")
     key.add(but_1, but_2, but_3)
     bot.send_message(message.chat.id, "Выберите кнопку", reply_markup=key)
+
+
+@bot.callback_query_handler(func=lambda c:True)
+def inline(c):
+  if c.data == 'NumberOne':
+    bot.send_message(c.message.chat.id, 'Это кнопка 1')
+  if c.data == 'NumberTwo':
+    bot.send_message(c.message.chat.id, 'Это кнопка 2')
+  if c.data == 'NumberTree':
+    key = types.InlineKeyboardMarkup()
+    but_1 = types.InlineKeyboardButton(text="NumberOne", callback_data="NumberOne")
+    but_2 = types.InlineKeyboardButton(text="NumberTwo", callback_data="NumberTwo")
+    but_3 = types.InlineKeyboardButton(text="NumberTree", callback_data="NumberTree")
+    key.add(but_1, but_2, but_3)
+    bot.send_message(c.message.chat.id, 'Это кнопка 3', reply_markup=key)
+
 
 @bot.message_handler(func=lambda message:True)
 def forward_handles(message):
@@ -30,11 +48,11 @@ def forward_handles(message):
         print('Exception in forward handler. Info: {}'.format(error))
 
 
-
 def main(use_logging, level_name):
     if use_logging:
         telebot.logger.setLevel((logging.getLevelName(level_name)))
     bot.polling(none_stop=True, interval=.5)
+
 
 if __name__ == '__main__':
     main(True, 'DEBUG')
