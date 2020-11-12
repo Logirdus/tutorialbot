e78d9a95d866bd2b921adc49ed1dd6d392c9f50e
Logirdus <regdust@yandex.ru>
2020-11-12 17:54:01 +0300

    weather bot for stepik. Форматирование кода. Добавлены заготовки для функций записи и чтения истории запросов. Пока история будет вестись только на время работы бота.

--- bot_weather.py
+++ bot_weather.py
@@ -4,22 +4,28 @@ token = '1428455785:AAH3qVi5lKSQ7mLHHZcrXcr49hRKXnmhaJc'
 
 bot = telebot.TeleBot(token)
 
-def wr_history(city, date = 'сегодня'):
+
+def add_history(city, date='сегодня'):
+    pass
+
+
+def read_history(city, date):
+    pass
 
 
 @bot.message_handler(func=lambda message: True)
 def weather(message):
     if message.text == '/start':
         bot.reply_to(message, 'Это бот-погода. Поможет узнать погоду в любом городе. Какой город интересует?')
-    elif message.text == 'Москва':
+    elif message.text.lower() == 'москва':
         bot.reply_to(message, 'Сейчас отличная погода!')
-        wr_history(message.text)
-    elif message.text == 'Москва завтра':
+        add_history(message.text)
+    elif message.text.lower() == 'москва завтра':
         bot.reply_to(message, 'Завтра еще лучше!')
-        wr_history(message.text)
+        add_history(message.text)
     elif 'история' in message.text.lower():
         req = [i for i in message.text.lower().split()]
-        print(message.text, req)
+        print(req)
         bot.reply_to(message, req)
     else:
         bot.reply_to(message, 'Я тебя не понял')
